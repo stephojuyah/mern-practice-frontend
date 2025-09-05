d1f760c878cfdddf0bbcd947eb94356dd89687b2
stephojuyah <stephojuyah2@gmail.com>
2025-09-05 20:23:28 +0100

    new commit

--- src/pages/reset_password/ResetPasswordCom1.jsx
+++ src/pages/reset_password/ResetPasswordCom1.jsx
@@ -7,6 +7,8 @@ const ResetPassword = () => {
             <div className='reset-form'>
                 <form action="" className='form-g'>
                     <h2 className='reset'>Reset Password</h2>
+                    <input type="number" className='otp-input' placeholder='Enter OTP' value={otp} required/>
+                    <input type="password" className='password-input' placeholder='New password' value={password} required/>
                 </form>
             </div>
         </div>
