dc1d2bd6354c5f79edbb6cacb0db3877231ad11a
stephojuyah <stephojuyah2@gmail.com>
2025-09-05 20:16:39 +0100

    new commit

--- src/pages/reset_password/ResetPasswordCom1.jsx
+++ src/pages/reset_password/ResetPasswordCom1.jsx
@@ -3,7 +3,13 @@ import '../../styles/reset_password.css'
 const ResetPassword = () => {
     return (
         <>
-        
+        <div className='reset-container'>
+            <div className='reset-form'>
+                <form action="" className='form-g'>
+                    <h2 className='reset'>Reset Password</h2>
+                </form>
+            </div>
+        </div>
         </>
     )
 }
