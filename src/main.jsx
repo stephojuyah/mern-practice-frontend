39965d03c13033c28acc0ee27aad331283f18c9d
stephojuyah <stephojuyah2@gmail.com>
2025-09-06 22:37:22 +0100

    new commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-// import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
 // import './index.css'
 // import App from './App.jsx'
 
-// import Register from './pages/register/Register.jsx'
-// import Login from './pages/login/Login.jsx'
-// import RequestReset from './pages/request_reset/RequestReset.jsx'
+import Register from './pages/register/Register.jsx'
+import Login from './pages/login/Login.jsx'
+import RequestReset from './pages/request_reset/RequestReset.jsx'
 // import ResetPassword from './pages/reset_password/ResetPassword.jsx'
-// import Home from './pages/home/Home.jsx'
-import Dash from './pages/dashboard/Dash.jsx'
+import Home from './pages/home/Home.jsx'
+// import Dash from './pages/dashboard/Dash.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
@@ -19,18 +19,18 @@ createRoot(document.getElementById('root')).render(
     {/* <RequestReset /> */}
     {/* <ResetPassword /> */}
     {/* <Home /> */}
-    <Dash />
+    {/* <Dash /> */}
 
 
-     {/* <BrowserRouter>
+     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/register" element={<Register />} />
         <Route path='/login' element={<Login />} />
-        <Route path='/home' element={<Dash />} />
+        {/* <Route path='/home' element={<Dash />} /> */}
         <Route path='/request_otp' element={<RequestReset />} />
-        <Route path='/reset_password' element={<ResetPassword />} />
+        {/* <Route path='/reset_password' element={<ResetPassword />} /> */}
       </Routes>   
-    </BrowserRouter> */}
+    </BrowserRouter>
   </StrictMode>,
 )
--- src/pages/register/RegisterCom1.jsx
+++ src/pages/register/RegisterCom1.jsx
@@ -1,37 +1,49 @@
 import '../../styles/register.css'
+import { useState } from "react"
 import { Link } from "react-router-dom";
 // import axios from "axios";
 
 const RegisterCom1 = () => {
 
-    const [name, setName] = useState("");
+  const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [phone_no, setPhoneNo] = useState("");
   const [password, setPassword] = useState("");
-  const [message, setMessage] = useState("");
+  const [msg, setMessage] = useState("");
+  const [agree, setAgree] = useState(false);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
 
     try {
-      const res = await axios.post("http://localhost:5000/auth/register", {
-        name,
-        email,
-        phone_no,
-        password,
-      });
+        
+        const res = await fetch("http://localhost:5000/auth/register", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            name,
+            email,
+            phone_no,
+            password,
+          }),
+        });
 
-      setMessage(res.data.msg || "Registration successful");
-      
+        const data = await res.json();
+        setMessage(data.msg || "Registration successful");
+
       // Clear fields on success
       setName("");
       setEmail("");
       setPhoneNo("");
       setPassword("");
-    } catch (err) {
-      setMessage(err.response?.data?.msg || "Something went wrong");
+      setAgree(false);
+    } catch (error) {
+      setMessage(err.res?.data?.msg || "Something went wrong");
     }
   };
+
 
 
     return (
@@ -41,15 +53,16 @@ const RegisterCom1 = () => {
                 <form className='form2' onSubmit={handleSubmit}>
                     <h2 className='get'>Getting Started</h2>
                     <p className='have'>Already have an account? <Link to="/login">Login</Link></p>
-                    <input value={name} onChange={(e) => setName(e.target.value)} className='input-box' type="text" name='name' placeholder='Name' required/><br />
-                    <input value={email} onChange={(e) => setEmail(e.target.value)} className='input-box' type="email" name='email' placeholder='Email' required/><br />
-                    <input value={phone_no} onChange={(e) => setPhoneNo(e.target.value)} className='input-box' type="tel" name='tel' placeholder='Phone number' required/><br />
-                    <input value={password} onChange={(e) => setPassword(e.target.value)} className='input-box' type="password" name='password' placeholder='Password' required/><br />
-                    <input value={password} onChange={(e) => setPassword(e.target.value)} className='input-box' type="password" name='password' placeholder='Confirm password' required/>
-                    <input className='create-link' type="submit" value='Create account'/>
+                    <input value={name} onChange={(e) => {setName(e.target.value); setMessage("")}} className='input-box' type="text" name='name' placeholder='Name' required/><br />
+                    <input value={email} onChange={(e) => {setEmail(e.target.value); setMessage("")}} className='input-box' type="email" name='email' placeholder='Email' required/><br />
+                    <input value={phone_no} onChange={(e) => {setPhoneNo(e.target.value); setMessage("")}} className='input-box' type="tel" name='tel' placeholder='Phone number' required/><br />
+                    <input value={password} onChange={(e) => {setPassword(e.target.value); setMessage("")}} className='input-box' type="password" name='password' placeholder='Password' required/><br />
+                    <input value={password} onChange={(e) => {setPassword(e.target.value); setMessage("")}} className='input-box' type="password" name='password' placeholder='Confirm password' required/>
+                    <p style={{textAlign: 'center', fontSize: '10px', marginTop: '0px'}}>{msg}</p>
+                    <input checked={agree} onChange={(e) => setAgree(e.target.checked)} className='create-link' type="submit" value='Create account'/>
                     {/* <a href="" className='create-link'>Create account</a> */}
                     <div className='check1'>
-                        <input className='check-button' type="checkbox" required/>
+                        <input onChange={(e) => setCheck(e.target.value)} className='check-button' type="checkbox" required/>
                         <span className='policy'>I accept the Terms of Use and acknowledge the <a href=''>Privacy Policy</a></span>
                     </div>
                 </form>
