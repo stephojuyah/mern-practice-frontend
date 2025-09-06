b44fd2a5a85d65615aa64ea7b21b374cf7ebf069
stephojuyah <stephojuyah2@gmail.com>
2025-09-06 20:35:16 +0100

    new commit

--- src/pages/register/RegisterCom1.jsx
+++ src/pages/register/RegisterCom1.jsx
@@ -4,18 +4,48 @@ import { Link } from "react-router-dom";
 
 const RegisterCom1 = () => {
 
+    const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone_no, setPhoneNo] = useState("");
+  const [password, setPassword] = useState("");
+  const [message, setMessage] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const res = await axios.post("http://localhost:5000/auth/register", {
+        name,
+        email,
+        phone_no,
+        password,
+      });
+
+      setMessage(res.data.msg || "Registration successful");
+      
+      // Clear fields on success
+      setName("");
+      setEmail("");
+      setPhoneNo("");
+      setPassword("");
+    } catch (err) {
+      setMessage(err.response?.data?.msg || "Something went wrong");
+    }
+  };
+
+
     return (
         <>
         <div className='register-container'>
             <div className='form'>
-                <form className='form2' action="">
+                <form className='form2' onSubmit={handleSubmit}>
                     <h2 className='get'>Getting Started</h2>
                     <p className='have'>Already have an account? <Link to="/login">Login</Link></p>
-                    <input className='input-box' type="text" name='name' placeholder='Name' required/><br />
-                    <input  className='input-box' type="email" name='email' placeholder='Email' required/><br />
-                    <input className='input-box' type="tel" name='tel' placeholder='Phone number' required/><br />
-                    <input className='input-box' type="password" name='password' placeholder='Password' required/><br />
-                    <input className='input-box' type="password" name='password' placeholder='Confirm password' required/>
+                    <input value={name} onChange={(e) => setName(e.target.value)} className='input-box' type="text" name='name' placeholder='Name' required/><br />
+                    <input value={email} onChange={(e) => setEmail(e.target.value)} className='input-box' type="email" name='email' placeholder='Email' required/><br />
+                    <input value={phone_no} onChange={(e) => setPhoneNo(e.target.value)} className='input-box' type="tel" name='tel' placeholder='Phone number' required/><br />
+                    <input value={password} onChange={(e) => setPassword(e.target.value)} className='input-box' type="password" name='password' placeholder='Password' required/><br />
+                    <input value={password} onChange={(e) => setPassword(e.target.value)} className='input-box' type="password" name='password' placeholder='Confirm password' required/>
                     <input className='create-link' type="submit" value='Create account'/>
                     {/* <a href="" className='create-link'>Create account</a> */}
                     <div className='check1'>
