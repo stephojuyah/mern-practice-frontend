a46c3c4774536856e1a6093bba4319a96c8bb409
stephojuyah <stephojuyah2@gmail.com>
2025-09-06 16:40:43 +0100

    new commit

--- src/pages/dashboard/DashCom1.jsx
+++ src/pages/dashboard/DashCom1.jsx
@@ -1,6 +1,22 @@
+import { useState } from "react"
 import "../../styles/dash.css"
 
 const DashCom1 = () => {
+    const [note, setNote] = useState('');
+    const [message, setMessage] = useState('')
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+         setMessage('Note submitted successfuly');
+         setNote('');
+    }
+
+    const handleChange = (e) => {
+  setNote(e.target.value);
+  if (message) setMessage('');
+};
+
+
     return (
         <>
         <div className="current-page">
@@ -9,9 +25,10 @@ const DashCom1 = () => {
                     <div><span className='scrib-home'>Scrabble</span></div>
                     <div className="main">
                         <h2 className="home-welcome">Welcome back, <br />Jane Doe</h2>
-                        <form action="" className="note-form">
-                            <textarea placeholder="Leave a note" required></textarea><br />
+                        <form onSubmit={handleSubmit} className="note-form">
+                            <textarea className='note' placeholder="Leave a note" onChange={handleChange} value={note} required></textarea><br />
                             <input className='submit-note' type="submit" value='Submit Note' />
+                            <p style={{textAlign: 'center'}}>{message}</p>
                         </form>
                     </div>
                 </div>
